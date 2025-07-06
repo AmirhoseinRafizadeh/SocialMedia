2eeadc7384ec10e4d6f70bacdab93c39bd5170ee
amirhosein rafizadeh <amirhosein.rafizadeh.dev@mail.com>
2025-07-06 20:35:45 +0330

    ep27 completed

--- socialMedia/socialMedia/settings.py
+++ socialMedia/socialMedia/settings.py
@@ -129,3 +129,12 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 LOGIN_URL = '/account/login/'
+
+#google account settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'amirhosein.rafizadeh.dev@gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_PASSWORD = 'srhjvaxhzvuojwcw'
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'amirhosein.rafizadeh.dev@gmail.com'
