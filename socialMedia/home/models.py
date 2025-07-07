06cc9e912f65d7ab631e94589e7ba1ab2f844c77
amirhosein rafizadeh <amirhosein.rafizadeh.dev@mail.com>
2025-07-07 13:33:17 +0330

    ep32 completed

--- socialMedia/home/models.py
+++ socialMedia/home/models.py
@@ -7,6 +7,8 @@ class Posts(models.Model):
     slug = models.SlugField()
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    class Meta:
+        ordering = ['-created']
     def __str__(self):
         return f'{self.slug} - {self.created}'
     def get_absolute_url(self):
